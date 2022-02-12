6506949204a46d9c5f3ca89d59237a1901677b77
remenok200 <remenok9@gmail.com>
2022-02-12 15:58:46 +0200

    add rest operator and arrow functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,16 @@
-'use strict';
+"use strict";
 
 const arr = new MyArray();
 const arr2 = new MyArray();
 const arr1 = new Array();
 
-arr.push(1);
-arr.push(3);
-arr.push(121);
-arr2.push('a');
-arr2.push('b');
+arr.push(1, 3, 121);
+arr2.push("a", "b");
 
-arr.forEach(function callbackfn(currentValue, index, array) {
-    return console.log(`Под индексом ${index} лежит ${currentValue}`);
+arr.forEach((currentValue, index) => {
+  return console.log(`Под индексом ${index} лежит ${currentValue}`);
 });
 
-let testMap = arr.map(function callbackfn(currentValue, index, array) {
-    return array[index] = index;
+let testMap = arr.map((currentValue, index, array) => {
+  return (array[index] = index);
 });
